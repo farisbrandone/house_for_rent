8797c23ce2401356722c8279f6efd90635919ea7
farisbrandone <farisbrandone@yahoo.com>
2024-07-05 06:02:51 +0100

    add key

--- components/CardsSkeleton.tsx
+++ components/CardsSkeleton.tsx
@@ -4,7 +4,7 @@ export function CardsSkeleton() {
   return (
     <div className="flex flex-wrap gap-4">
       {Array(8).map((_, index: number) => (
-        <div className="flex flex-col space-y-3">
+        <div className="flex flex-col space-y-3" key={index}>
           <Skeleton className="h-[125px] w-[250px] rounded-xl" />
           <div className="space-y-2">
             <Skeleton className="h-4 w-[250px]" />
