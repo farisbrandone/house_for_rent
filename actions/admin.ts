9c81a21928567845989da344681973d9acb8053b
farisbrandone <farisbrandone@yahoo.com>
2024-07-13 20:08:06 +0100

    modify admin.ts action file

--- actions/admin.ts
+++ actions/admin.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { currentRole } from "@/lib/auth";
-import { UserRole } from "@prisma/client";
+import * as dd from "@prisma/client";
 
 export const admin = async () => {
   const role = await currentRole();
 
-  if (role === UserRole.ADMIN) {
+  if (role === dd.UserRole.ADMIN) {
     return { success: "Allowed Server Action!" };
   }
 
