75e29c794b7868dfb353a2b211ed6ea9ef33615f
farisbrandone <farisbrandone@yahoo.com>
2024-07-14 04:33:29 +0100

    change18

--- next.config.js
+++ next.config.js
@@ -16,6 +16,9 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 };
 
 module.exports = nextConfig;
