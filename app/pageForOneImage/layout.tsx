90c28708c29689edeb6958e49d350c1a0bf15dd7
farisbrandone <farisbrandone@yahoo.com>
2024-07-17 07:38:56 +0100

    delet

---
+++ app/pageForOneImage/layout.tsx
@@ -0,0 +1,30 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Metadata } from "next";
+import React, { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "MaMaisonALouer.com",
+  description: "Le meilleur site pour la recherche de vos offres immobilières",
+  icons: {
+    icon: "/vercel.svg",
+  },
+};
+
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <div className="w-full bg-[linear-gradient(40deg,#006ce4,#003b95)] flex items-center justify-center mt-0">
+        <Header searchOrNot={false} headerForSign={false} />
+      </div>
+      <div className="flex items-center justify-center">
+        {/* <Suspense>{children}</Suspense>*/}
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
---
+++ app/pageForOneImage/page.tsx
@@ -0,0 +1,29 @@
+import CardOneOffer from "@/components/CardOneOffer";
+import { CardsSkeleton } from "@/components/CardsSkeleton";
+
+import { getOfferByUserId } from "@/data/offer";
+import React, { Suspense } from "react";
+
+const DataForOffer = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    id?: string;
+  };
+}) => {
+  const myId = searchParams?.id;
+
+  const dataForOneOffer = await getOfferByUserId(myId);
+
+  if (!myId) {
+    throw new Error("une ereur est survenu pendant le processus de traitement");
+  }
+
+  return (
+    <div className="mt-5 lg:w-[1024px]">
+      <CardOneOffer dataForOneOffer={dataForOneOffer} />
+    </div>
+  );
+};
+
+export default DataForOffer;
