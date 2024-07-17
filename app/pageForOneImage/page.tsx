e92f90001d005ff9972d6cb3119453136695c9de
farisbrandone <farisbrandone@yahoo.com>
2024-07-17 07:59:49 +0100

    de

--- app/pageForOneImage/page.tsx
+++ app/pageForOneImage/page.tsx
@@ -1,18 +1,20 @@
-import CardOneOffer from "@/components/CardOneOffer";
-import { CardsSkeleton } from "@/components/CardsSkeleton";
+//import CardOneOffer from "@/components/CardOneOffer";
+//import { CardsSkeleton } from "@/components/CardsSkeleton";
 
-import { getOfferByUserId } from "@/data/offer";
-import React, { Suspense } from "react";
+//import { getOfferByUserId } from "@/data/offer";
+import React from "react";
 
-const DataForOffer = async ({}: /*searchParams,*/
-{
-  /* searchParams?: {
+const DataForOffer = async (
+  /*searchParams,*/
+  {
+    /* searchParams?: {
     id?: string;
   };*/
-}) => {
+  }
+) => {
   // const myId = searchParams?.id;
 
-  const dataForOneOffer = await getOfferByUserId("2");
+  //const dataForOneOffer = await getOfferByUserId("2");
 
   /*if (!myId) {
     throw new Error("une ereur est survenu pendant le processus de traitement");
@@ -20,7 +22,7 @@ const DataForOffer = async ({}: /*searchParams,*/
 
   return (
     <div className="mt-5 lg:w-[1024px]">
-      <CardOneOffer dataForOneOffer={dataForOneOffer} />
+      {/*<CardOneOffer dataForOneOffer={dataForOneOffer} />*/}
     </div>
   );
 };
