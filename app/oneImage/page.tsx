e92111edabe27e86496232f7a4aec866de3dfa0b
farisbrandone <farisbrandone@yahoo.com>
2024-07-17 08:18:53 +0100

    abassas

--- app/oneImage/page.tsx
+++ app/oneImage/page.tsx
@@ -1,22 +1,27 @@
-//import CardOneOffer from "@/components/CardOneOffer";
-//import { CardsSkeleton } from "@/components/CardsSkeleton";
+import CardOneOffer from "@/components/CardOneOffer";
+import { CardsSkeleton } from "@/components/CardsSkeleton";
 
-//import { getOfferByUserId } from "@/data/offer";
+import { getOfferByUserId } from "@/data/offer";
 import React from "react";
 
-const page = async () => {
-  // const myId = searchParams?.id;
+const page = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    id?: string;
+  };
+}) => {
+  const myId = searchParams?.id;
 
-  //const dataForOneOffer = await getOfferByUserId("2");
+  const dataForOneOffer = await getOfferByUserId(myId);
 
-  /*if (!myId) {
+  if (!myId) {
     throw new Error("une ereur est survenu pendant le processus de traitement");
-  }*/
+  }
 
   return (
     <div className="mt-5 lg:w-[1024px]">
-      patatoes
-      {/*<CardOneOffer dataForOneOffer={dataForOneOffer} />*/}
+      <CardOneOffer dataForOneOffer={dataForOneOffer} />
     </div>
   );
 };
